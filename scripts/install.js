1f3c4e578f05968bdb86aa70d17608e424a6202b
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-06 14:04:02 +0100

    removed npm as hard dependency, fetching remotely from raw github files

--- scripts/install.js
+++ scripts/install.js
@@ -1,72 +1,32 @@
-#!/usr/bin/env node
 
-var path = require('path'),
-  semver = require('semver'),
-  exec = require('child_process').exec,
-  fs = require('fs'),
-  console = require('../lib/logger');
+var fs = require('fs'),
+  path = require('path'),
+  rimraf = require('rimraf'),
+  gh = require('gh-fetch'),
+  log = require('../lib/logger');
 
-var prefix = path.join(process.env.HOME, '.gimme-assets');
+var prefix = path.join(process.env.HOME, '.gimme');
 
-// use the http clone url, works better with proxies (should be made configurable)
+log.log('run newinstall', prefix);
+
+// What shis script shoul do
+//
+// * wipe out previous ~/.gimme dir if any
+// * Grab latest copy of https://raw.github.com/cdnjs/website/gh-pages/packages.json
+// * Store anc cache it in ~/.gimme
+//
 
 
-// On package install, clone the cdnjs repo into ~/.gimme-assets/cdnjs
+// Wipe out previous ~/.gimme if any
+rimraf.sync(prefix);
 
-// * todo: clone/pull if directory exists, instead of raw remove
-// * todo: validate each pacakge.json, edit version to be semver compliant if necessary (add a `.x` to versions like `1.0`)
+// fetch latest copy of cdnjs' packages
+var fetcher = gh.fetch(['cdnjs/website', 'packages.json'], { branch: 'gh-pages', whereto: prefix }, function(err) {
+  log.debug('fetch done', arguments);
 
-// Get the git http.proxy config, if set then use git over http, otherwise standard git clone.
-exec('git config --global http.proxy', function(err, stdout) {
-  var url = 'git://github.com/cdnjs/cdnjs.git';
-  if(err && err.code !== 1) throw err;
-  if(stdout.trim()) url = 'https://github.com/cdnjs/cdnjs.git';
-  install(url);
+  // Try to parse its content
+  var pkgs = JSON.parse(fs.readFileSync(path.join(prefix, 'packages.json'), 'utf8'));
+  console.log(pkgs);
+
 });
-
-
-function install(url) {
-  exec('rm -rf ' + prefix, function(err) {
-    mkdirp(prefix, function(err) {
-      if(err) throw err;
-
-      console.info('Cloning cdnjs repository from ', url);
-      exec('git clone ' + url + ' ' + path.join(prefix, 'cdnjs'), function(err, stdout, stderr) {
-        if(err) throw err;
-        console.debug(stdout, stderr);
-        console.info(' ✔ Cloned cdnjs repo in ~/.gimme-assets');
-
-        console.debug('Checking out package.json version against semver, adding necessary `.x` on `1.0` if needed.');
-
-        var libs = path.join(prefix, 'cdnjs', 'ajax', 'libs');
-
-        fs.readdirSync(libs)
-          .filter(function(file) {
-            return fs.statSync(path.join(libs, file)).isDirectory();
-          })
-          .forEach(function(file) {
-            var pkgpath = path.join(libs, file, 'package.json'),
-              pkg = JSON.parse(fs.readFileSync(pkgpath)),
-              version = pkg.version,
-              parts = pkg.version.split('.');
-
-            if(!semver.satisfies(version, '*')) {
-              if(parts.length !== 2) return console.warn('Invalid version field: ', pkg.version, 'for', pkg.name);
-              console.info('Updating version', version, 'for ', pkg.name);
-
-              // also needs to concat the last minor version part to package folders too(0.6, 0.7, etc.)
-              pkg.version = parts.concat('0').join('.');
-
-              console.debug('Writing new pkg:', pkgpath, version, ' → ', pkg.version);
-              fs.writeFileSync(pkgpath, JSON.stringify(pkg, null, 2))
-            }
-          });
-      });
-    });
-  });
-}
-
-function mkdirp(path, cb) {
-  return exec('mkdir -p ' + path, cb);
-}
 
