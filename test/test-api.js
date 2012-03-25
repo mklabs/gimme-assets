5a381c68b6bec974fbe656c28eee75ff2fa33390
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-25 01:07:32 +0100

    test - split test-creation into its own file

--- test/test-api.js
+++ test/test-api.js
@@ -41,39 +41,3 @@ assert.ok(gimme.info('same goes for config interface').get())
 assert.equal(gimme.set('foobar', 'bar').get('foobar'), 'bar');
 assert.equal(gimme.config.env(), 'development');
 
-
-
-//
-// App creation
-// ------------
-//
-gimme.info('Should be able to create new app with gimme.create');
-var app = gimme.create();
-app.info('And this should be another instance');
-assert.ok(app !== gimme);
-
-//
-// ## Testing events
-//
-var to = setTimeout(function() { assert.fail('Fail!'); }, 500);
-var init = setTimeout(function() { assert.fail('Fail!'); }, 500);
-var start = setTimeout(function() { assert.fail('Fail!'); }, 500);
-app.on('SAVE THE TESTS!!', function() {
-  clearTimeout(to);
-});
-
-app.on('init', function() {
-  gimme.debug('gimme should emit an `init` event when started');
-  clearTimeout(init);
-});
-
-app.on('start', function() {
-  gimme.debug('gimme should emit a `start` event when started');
-  clearTimeout(start);
-});
-
-app.on('end', app.emit.bind(app, 'SAVE THE TESTS!!'));
-
-// start!
-app.start();
-
